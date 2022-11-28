7cc1c289e168a805f61405c57802db2f18d6db32
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-28 22:57:27 +0300

    Created middleware verifyAuthToken

---
+++ src/middleware/auth-token.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const verifyAuthToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader?.split(' ')[1];
+    const decoded = jwt.verify(token as string, process.env.TOKEN as string);
+    if (decoded) {
+      next();
+    } else {
+      res.status(401);
+    }
+  } catch (error) {
+    res.status(401);
+  }
+};
