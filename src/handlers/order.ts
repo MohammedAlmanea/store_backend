397ad40356e6e31bd9a0ca01ea0722dabe425a28
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-28 22:37:22 +0300

    Created order handlers

---
+++ src/handlers/order.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import { Order, Order_Class } from '../models/orders';
+
+const OrderObj = new Order_Class();
+
+
+const show = async (req: Request, res: Response) => {
+  try {
+    const order = await OrderObj.show(req.params.id);
+    res.json(order);
+  } catch (error) {
+    res.status(400);
+    res.json(error);
+  }
+};
+
+const OrdersRoutes = (app: express.Application) => {
+  app.get('/orders/:id', show);
+};
+
+export default OrdersRoutes;
