6d1b58a38be74d6790e008864aaf59c6620b5533
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-30 15:36:37 +0300

    Added handling

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -1,19 +1,44 @@
-import {client} from '../database';
+import { client } from '../database';
 
 export type Order = {
   id?: number;
   status: string;
-  user_id: number;
+  user_id: string;
 };
 
 export class Order_Class {
-  async show(userId: string): Promise<Order> {
+  async create(or: Order): Promise<Order> {
     try {
-      const sql = 'SELECT * FROM orders WHERE id=($1)';
+      const sql =
+        'INSERT INTO orders (status, user_id) VALUES($1, $2) RETURNING *';
+
+      const conn = await client.connect();
+
+      const result = await conn.query(sql, [or.status, or.user_id]);
+
+      const order = result.rows[0];
+
+      conn.release();
+
+      return order;
+    } catch (err) {
+      throw new Error(`Could not add new order. Error: ${err}`);
+    }
+  }
+
+  async show(userId: string): Promise<Order[]|null> {
+    try {
+      const sql = 'SELECT * FROM orders WHERE user_id=($1)';
       const conn = await client.connect();
       const result = await conn.query(sql, [userId]);
       conn.release();
-      return result.rows[0];
+      if (result.rows.length)
+      {
+      return result.rows;
+      }
+      else {
+        return null
+      }
     } catch (error) {
       throw new Error(`Can't get order. ${error}`);
     }
