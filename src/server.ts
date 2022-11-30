0af1bfb12d2a04ff9b128d8b27ada1e141286b06
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-30 15:33:33 +0300

    Added imports to all handlers routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import OrdersRoutes from './handlers/order';
+import userRoutes from './handlers/user';
+import productsRoutes from './handlers/product';
+
 
 
 const app: express.Application = express();
@@ -11,7 +15,12 @@ app.get('/', (_req:Request,res:Response) =>{
   res.send('Welcome')
 });
 
+userRoutes(app);
+OrdersRoutes(app);
+productsRoutes(app);
 
 app.listen(3000, function () {
   console.log(`starting app on: http://localhost:${port}`);
 });
+
+export default app;
