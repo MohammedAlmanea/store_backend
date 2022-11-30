b9177e4ef2f3eb3850ab16f924d91f4340b34a86
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-30 15:43:37 +0300

    Created order model spec

---
+++ src/models/tests/ordersSpec.ts
@@ -0,0 +1,33 @@
+import { Order_Class} from "../orders";
+
+const orderObj = new Order_Class();
+
+describe('Order model', () => {
+  it('should have a show method', () => {
+    expect(orderObj.show).toBeDefined();
+  });
+  it('should have a create method', () => {
+    expect(orderObj.create).toBeDefined();
+  });
+
+  it('create method should create an order', async () => {
+    const result = await orderObj.create({
+      status: 'open',
+      user_id: '1'
+    });
+    expect(result).toEqual({
+      id: 1,
+      status: 'open',
+      user_id: '1'
+    });
+  });
+
+  it('show method should show the all orders made by the user using his user_id ', async () => {
+    const result = await orderObj.show('1');
+    expect(result).toEqual([{
+      id: 1,
+      status:'open',
+      user_id: '1'
+    }]);
+  });
+});
