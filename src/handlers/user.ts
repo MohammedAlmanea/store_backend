55605f46d41ee3c8e37160a1122c389537d9231c
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-30 15:38:53 +0300

    Created handling if there is no user

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -7,13 +7,29 @@ import { verifyAuthToken } from '../middleware/auth-token';
 const Table = new UserTable();
 
 const index = async (_req: Request, res: Response) => {
+  try {
   const users = await Table.index();
   res.json(users);
+  } catch (error) {
+    res.status(400);
+    res.json(`Can't get users!: ${error}`);
+  }
 };
 
 const show = async (_req: Request, res: Response) => {
-  const user = await Table.show(_req.params.id);
-  res.json(user);
+  try {
+    const user = await Table.show(_req.params.id);
+    if (user !== null) {
+    res.json(user);
+    }
+    else{
+      res.status(404);
+      res.json(`There is no user with this id!`)
+    }
+  } catch (error) {
+    res.status(400);
+    res.json(`Unable to find the user!: ${error}`);
+  }
 };
 
 const create = async (_req: Request, res: Response) => {
@@ -21,36 +37,41 @@ const create = async (_req: Request, res: Response) => {
     const user: User = {
       first_name: _req.body.first_name,
       last_name: _req.body.last_name,
-      user_name: _req.body.user_name,
       password: _req.body.password,
     };
 
     const newUser = await Table.create(user);
-    var token = jwt.sign(newUser ,process.env.TOKEN as string)
+    const token = jwt.sign(newUser ,process.env.TOKEN as string)
     res.json(token);
   } catch (err) {
     res.status(400);
-    res.json(err);
+    res.json(`${err}`);
   }
 };
 
 
 const authenticate = async (_req: Request, res: Response) => {
   try {
-    const username = _req.body.user_name;
+    const last_name = _req.body.last_name;
     const password = _req.body.password;
 
-    const auth = await Table.authenticate(username, password);
-    var token = jwt.sign({auth} ,process.env.TOKEN as string)
-    res.json(token);
+    const auth = await Table.authenticate(last_name, password);
+    if(auth!== null){
+    const token = jwt.sign({auth} ,process.env.TOKEN as string)
+    res.status(200).json({token})
+    }
+    else {
+      res.status(409);
+      res.json('Not auth!!');
+    }
   } catch (err) {
     res.status(401);
-    res.json(err);
+    res.json(`${err}`);
   }
 };
 
 const userRoutes = (app: express.Application) => {
-  app.post('/users', verifyAuthToken, create);
+  app.post('/users', create);
   app.post('/users/authenticate', authenticate);
   app.get('/users',verifyAuthToken, index);
   app.get('/users/:id',verifyAuthToken, show);
